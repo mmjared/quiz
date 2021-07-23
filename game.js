fb9c59144165df0cd79a3fff603fef366332ee66
jared macias <mmjared11@outlook.com>
2021-07-23 14:54:00 -0600

    working on gamejs

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const proBarFull = document.querySelector('#proBarFull');
 
-let currentQuestion = ();
+let currentQuestion = {};
 let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
@@ -12,4 +12,75 @@ let availableQuestions = [];
 
 let question = [{
 
+    question: 'what is 2 + 2?',
+    choice1: '2',
+    choice2: '4',
+    choice3: '6',
+    choice4: '8',
+    answer: 4,
 }]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+
+}
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+
+    }
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX-QUESTION}`
+    proBarFull.style.width = `${(questionCounter/MAX_QUESTION) * 100}%`
+
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex]
+    question.innerText = currentQuestion.question
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionIndex, 1)
+
+    acceptingAnswers = true
+
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if (classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+            se
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score += num
+    scoreText.innerText = score
+}
+startgame()
