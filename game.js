602ad88cbced16b0dae1aa7da8fc5c1aab91158c
jared macias <mmjared11@outlook.com>
2021-07-23 18:02:33 -0600

    fixed js bugs

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
-const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
-const progressText = document.querySelector('#progressText');
-const scoreText = document.querySelector('#score');
-const proBarFull = document.querySelector('#proBarFull');
+const question = document.querySelector("#question");
+const choices = Array.from(document.querySelectorAll(".choice-text"));
+const progressText = document.querySelector("#progressText");
+const scoreText = document.querySelector("#score");
+const proBarFull = document.querySelector("#proBarFull");
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -10,77 +10,74 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let question = [{
-
-    question: 'what is 2 + 2?',
-    choice1: '2',
-    choice2: '4',
-    choice3: '6',
-    choice4: '8',
+let questions = [{
+    question: "what is 2 + 2?",
+    choice1: "2",
+    choice2: "4",
+    choice3: "6",
+    choice4: "8",
     answer: 4,
-}]
+}, ];
 
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 4
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 4;
 
 startGame = () => {
-    questionCounter = 0
-    score = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
-
-}
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
 
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem("mostRecentScore", score);
 
-        return window.location.assign('/end.html')
-
+        return window.location.assign("/end.html");
     }
-    questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX-QUESTION}`
-    proBarFull.style.width = `${(questionCounter/MAX_QUESTION) * 100}%`
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    proBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionIndex]
-    question.innerText = currentQuestion.question
-    choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
-    })
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+    choices.forEach((choice) => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+    });
 
-    availableQuestions.splice(questionIndex, 1)
+    availableQuestions.splice(questionIndex, 1);
 
-    acceptingAnswers = true
+    acceptingAnswers = true;
+};
 
-}
+choices.forEach((choice) => {
+    choice.addEventListener("click", (e) => {
+        if (!acceptingAnswers) return;
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if (!acceptingAnswers) return
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
 
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
+        let classToApply =
+            selectedAnswer === currentQuestion.answer ? "correct" : "incorrect";
 
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-
-        if (classToApply === 'correct') {
-            incrementScore(SCORE_POINTS)
+        if (classToApply === "correct") {
+            incrementScore(SCORE_POINTS);
         }
 
-        selectedChoice.parentElement.classList.add(classToApply)
+        selectedChoice.parentElement.classList.add(classToApply);
         setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
-            se
-        }, 1000)
-    })
-})
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    });
+});
 
-incrementScore = num => {
-    score += num
-    scoreText.innerText = score
-}
-startgame()
+incrementScore = (num) => {
+    score += num;
+    scoreText.innerText = score;
+};
+
+startGame();
